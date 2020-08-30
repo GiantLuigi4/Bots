c9862bbad283fa3dc83609ffdadbc61ede93d1e9
GiantLuigi4 <tjmferet@hotmail.com>
2020-08-30 16:46:33 -0400

    now it creates bots.properties if it does not exist.

--- src/main/java/bots/BunchOBots.java
+++ src/main/java/bots/BunchOBots.java
@@ -4,6 +4,7 @@ import bots.idle_maker.IdleMkr;
 import bots.rater.PublicRaterBot;
 import bots.rater.RaterBot;
 import bots.role_reaction.RoleReactionBot;
+import utils.Files;
 import utils.PropertyReader;
 
 import javax.security.auth.login.LoginException;
@@ -12,18 +13,20 @@ public class BunchOBots {
 	public static final String drive = PropertyReader.read("settings.properties","drive");
 	
 	public static void main(String[] args) throws LoginException {
-		if (PropertyReader.contains("bots.properties","roleReaction")) {
-			RoleReactionBot.main(args);
-		}
-		if (PropertyReader.contains("bots.properties","rbPrivate")) {
-			RaterBot.main(args);
-		} else {
-			if (PropertyReader.contains("bots.properties","rbPublic")) {
-				PublicRaterBot.main(args);
+		if (!Files.create("bots.properties")) {
+			if (PropertyReader.contains("bots.properties","roleReaction")) {
+				RoleReactionBot.main(args);
 			}
-		}
-		if (PropertyReader.contains("bots.properties","idleMaker")) {
-			IdleMkr.main(args);
+			if (PropertyReader.contains("bots.properties","rbPrivate")) {
+				RaterBot.main(args);
+			} else {
+				if (PropertyReader.contains("bots.properties","rbPublic")) {
+					PublicRaterBot.main(args);
+				}
+			}
+			if (PropertyReader.contains("bots.properties","idleMaker")) {
+				IdleMkr.main(args);
+			}
 		}
 	}
 }
--- src/main/java/utils/Files.java
+++ src/main/java/utils/Files.java
@@ -1,5 +1,21 @@
 package utils;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Files {
 	public static final String dir = System.getProperty("user.dir");
+	
+	public static boolean create(String file) {
+		File f = new File(dir + "\\" + file);
+		if (!f.exists()) {
+			try {
+				f.getParentFile().mkdirs();
+				f.createNewFile();
+				return true;
+			} catch (Throwable ignored) {
+			}
+		}
+		return false;
+	}
 }
